a58a241695ce08e6f567abe92ceb671140195b58
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-08 17:26:54 +0200

    add routes for new event

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'users/sessions'}
   root to: 'events#index'
   resources :users
+  resources :events
+  post 'events/new', to: 'events#create'
 end
