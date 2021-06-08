ca92e5d920fb7d7de5e61c90bc13f8b1b445b65c
od-c0d3r <omar_rashad@aol.com>
2021-06-08 15:12:51 +0200

    Create Event<=>User Association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_123923) do
+ActiveRecord::Schema.define(version: 2021_06_08_124643) do
 
   create_table "events", force: :cascade do |t|
     t.integer "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
