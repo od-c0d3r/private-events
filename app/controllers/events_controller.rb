e7876cb204747eb0ac2372efecf445d4d1861c4f
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-08 16:18:17 +0200

    add event form template

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,19 @@ class EventsController < ApplicationController
   def index
 
   end
+
+  def create
+    @event = current_user.build_event(user_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private 
+
+def user_params
+   params.require(:event).permit(:date, :location) 
 end
