d58b492863b989f9431a2a74daa12c8586d903e9
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-11 15:43:43 +0200

    Remove functions from users helper and place it in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(user_params)
     @user_events = @user.events
+    @user_future_events = @user.attended_events.upcoming
+    @user_past_events = @user.attended_events.past
+
   end
+
+
 end
+
 
 private
 
