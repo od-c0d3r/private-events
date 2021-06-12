8c03779e76eb546b0145eb2e844b649ff9416b3f
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-12 21:16:23 +0200

    Remove validation to create past event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,10 +7,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: :User
 
   validates :date, presence: true
-  validate :create_recent_date
+
   validates :location, presence: true
 
-  def create_recent_date
-    errors.add(:date, "can't be in the past") if date.present? && date < Date.today
-  end
 end
